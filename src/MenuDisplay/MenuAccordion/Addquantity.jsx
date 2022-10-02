c9267998f1e50da2acf4e5db98c5d40422536040
Gokul <gokuljune2003@gmail.com>
2022-10-02 22:23:29 +0530

    made a changes

--- src/MenuDisplay/MenuAccordion/Addquantity.jsx
+++ src/MenuDisplay/MenuAccordion/Addquantity.jsx
@@ -56,7 +56,7 @@ function AddQuantity(props){
     <div style={style.maindiv} className="container-sm ">
         <button  style={style.btn}><span title="add" onClick={props.add}  style={style.btnspan}>+</span></button>
         <span style={style.spantext}>{props.quantity}</span>
-        {props.quantity=="1"?<button  style={style.btn}><span title="sub" onClick={props.remove} style={style.delbtn}><svg xmlns="http://www.w3.org/2000/svg" width="12" height="12" style={{position:'relative',top:'-.1rem'}} fill="currentColor" class="bi bi-trash-fill" viewBox="0 0 16 16">
+        {props.quantity==="1"?<button  style={style.btn}><span title="sub" onClick={props.remove} style={style.delbtn}><svg xmlns="http://www.w3.org/2000/svg" width="12" height="12" style={{position:'relative',top:'-.1rem'}} fill="currentColor" class="bi bi-trash-fill" viewBox="0 0 16 16">
   <path d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1H2.5zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5zM8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5zm3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0z"/>
 </svg></span></button>:<button  style={style.btn}><span title="sub" onClick={props.sub} style={style.btnspan}>-</span></button>}
         
