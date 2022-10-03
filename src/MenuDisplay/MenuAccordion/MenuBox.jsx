81fd1a287557be5e936fe13ff005e02ab44a3e55
Gokul <gokuljune2003@gmail.com>
2022-10-03 11:06:44 +0530

    clear a small bug

--- src/MenuDisplay/MenuAccordion/MenuBox.jsx
+++ src/MenuDisplay/MenuAccordion/MenuBox.jsx
@@ -1,11 +1,14 @@
-import React, {  useEffect, useState } from "react";
-import 'bootstrap/dist/css/bootstrap.min.css';
-import img from './image.png'
-import './MenuBox.css'
+import React, { useEffect, useState } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import img from "./image.png";
+import "./MenuBox.css";
 import AddQuantity from "./Addquantity";
 import { useDispatch, useSelector } from "react-redux";
-import { addorders} from "../../redux/feature/order/Orders";
-import { decrement, increment } from "../../redux/feature/Quantity/quantitySlice";
+import { addorders } from "../../redux/feature/order/Orders";
+import {
+  decrement,
+  increment,
+} from "../../redux/feature/Quantity/quantitySlice";
 import { addamount, reduceamount } from "../../redux/feature/total/totalSlice";
 import { infomcartclick } from "../../redux/Global/globalstate";
 // import { RemoveOrderfromarray } from "../../cart/cart";
@@ -16,155 +19,178 @@ import { infomcartclick } from "../../redux/Global/globalstate";
 // import { useDispatch,useSelector } from "react-redux";
 // import {  addorders} from "../../redux/feature/order/Orders";
 const style = {
-    image:{
-    width:'60px',
-    heigth:'60px'
-    },
-    addbtn:{
-        heigth:'60px'
-    },
-    note:{
-        fontSize:"12px"
-    }
-}
+  image: {
+    width: "60px",
+    heigth: "60px",
+  },
+  addbtn: {
+    heigth: "60px",
+  },
+  note: {
+    fontSize: "12px",
+  },
+};
 var total = 0;
 export const MenuBox = (props) => {
-    const should_blur = useSelector(state=>state.Gstate.iscartclicked)
+  const should_blur = useSelector((state) => state.Gstate.iscartclicked);
 
-//     function Toast(){
-//         // <!-- Flexbox container for aligning the toasts -->
-// <div aria-live="polite" aria-atomic="true" class="d-flex justify-content-center align-items-center w-100">
+  //     function Toast(){
+  //         // <!-- Flexbox container for aligning the toasts -->
+  // <div aria-live="polite" aria-atomic="true" class="d-flex justify-content-center align-items-center w-100">
 
-//   {/* <!-- Then put toasts within --> */}
-//   <div class="toast" role="alert" aria-live="assertive" aria-atomic="true">
-//     <div class="toast-header">
-//       <img src="..." class="rounded me-2" alt="..."/>
-//       <strong class="me-auto">Bootstrap</strong>
-//       <small>11 mins ago</small>
-//       <button type="button" class="btn-close" data-bs-dismiss="toast" aria-label="Close"></button>
-//     </div>
-//     <div class="toast-body">
-//       Hello, world! This is a toast message.
-//     </div>
-//   </div>
-// </div>
-//     }
-    const price = 5;
-    const dispatch = useDispatch()
-    const itemName = props.name
-    // const dispatch = useDispatch()
-    const cartquantity = useSelector(state=>state.quantity.value)
+  //   {/* <!-- Then put toasts within --> */}
+  //   <div class="toast" role="alert" aria-live="assertive" aria-atomic="true">
+  //     <div class="toast-header">
+  //       <img src="..." class="rounded me-2" alt="..."/>
+  //       <strong class="me-auto">Bootstrap</strong>
+  //       <small>11 mins ago</small>
+  //       <button type="button" class="btn-close" data-bs-dismiss="toast" aria-label="Close"></button>
+  //     </div>
+  //     <div class="toast-body">
+  //       Hello, world! This is a toast message.
+  //     </div>
+  //   </div>
+  // </div>
+  //     }
+  const price = 5;
+  const dispatch = useDispatch();
+  const itemName = props.name;
+  // const dispatch = useDispatch()
+  const cartquantity = useSelector((state) => state.quantity.value);
 
-    // const[total,settotal] = useState(0)
-    // const finalyarray = useSelector(state=>state.orders.FinalyOrderArray)
-    // const[index,setindex]=useState(0)
-    const orderarr = useSelector(state=>state.orders.OrderArray)
-    const[added,setadd]=useState(false)
-    const[quantity,setquantity] = useState(0)
-    const[selected,setselected]=useState(false)
-    const cart_reset = useSelector(state=>state.cart_reset.value)
-    // const orderarr=[Name,quantity]
-    // const[order,setorder] = useState({Name,quantity,price})
-    const[prevtotal,setprevioustotal] = useState(0)
-    const cartitems = useSelector(state=>state.orders.OrderArray)
-    // console.log("checking exported cartitems: ",cartitems);
-   
-    function handleinc(){
+  // const[total,settotal] = useState(0)
+  // const finalyarray = useSelector(state=>state.orders.FinalyOrderArray)
+  // const[index,setindex]=useState(0)
+  const orderarr = useSelector((state) => state.orders.OrderArray);
+  const [added, setadd] = useState(false);
+  const [quantity, setquantity] = useState(0);
+  const [selected, setselected] = useState(false);
+  const cart_reset = useSelector((state) => state.cart_reset.value);
+  // const orderarr=[Name,quantity]
+  // const[order,setorder] = useState({Name,quantity,price})
+  const [prevtotal, setprevioustotal] = useState(0);
+  const cartitems = useSelector((state) => state.orders.OrderArray);
+  // console.log("checking exported cartitems: ",cartitems);
 
-        setselected(true)
+  function handleinc() {
+    setselected(true);
 
-        
-        setquantity(quantity+1)
-       
-       
-        setprevioustotal(total)
+    setquantity(quantity + 1);
+
+    setprevioustotal(total);
     //    dispatch(updatequantity({itemName,quantity}))
-       
-        console.log("Testing: ",orderarr[1]);
-        // settotal(total * price)
-        // console.log('Quantity: ',quantity);
-        // console.log("Price: ",price);
-        // console.log("Total Price of "+{Name}+" is: ",total);
-        // dispatch(addamount(total))
-       
+
+    console.log("Testing: ", orderarr[1]);
+    // settotal(total * price)
+    // console.log('Quantity: ',quantity);
+    // console.log("Price: ",price);
+    // console.log("Total Price of "+{Name}+" is: ",total);
+    // dispatch(addamount(total))
+  }
+
+  function handledec() {
+    if (quantity !== 0) {
+      setquantity(quantity - 1);
     }
-    
-    function handledec(){
-        
-        if(quantity!==0){setquantity(quantity-1)}
-        // if(quantity!==0&&quantity!==1){setquantity(quantity-1)}
-       
-       
-
+    // if(quantity!==0&&quantity!==1){setquantity(quantity-1)}
+  }
+  function handledelete() {
+    setselected(false);
+    setadd(false);
+    setadd(false);
+    setquantity(0);
+    // RemoveOrderfromarray(itemName)
+    if (cartquantity > 0) {
+      dispatch(decrement());
     }
-    function handledelete(){
-        setselected(false)
-        setadd(false)
-        setadd(false)
-        setquantity(0)
-        // RemoveOrderfromarray(itemName)
-        if(cartquantity>0){
-        dispatch(decrement())
-        }
-        dispatch(infomcartclick())
+    dispatch(infomcartclick());
 
-        // dispatch(removeOrder(props.name))
-    }
-    useEffect(()=>{
-        setquantity(quantity)
-        // setorder({Name,quantity})
-    },[quantity,itemName,price])
-    useEffect(()=>{
-        setquantity(0)
-        setselected(false)
-        setadd(false)
-    },[cart_reset])
-    //------------------------------------ Handle Add Function ---------------------------------------------------------//
-    function handleadd(){
-        
-        dispatch(increment())
-        setadd(true)
-        // console.log('order: ',order);
-        total=quantity*price;
-        dispatch(reduceamount(prevtotal))
-        dispatch(addamount(total))
-        dispatch(addorders({ITEM:{Name:itemName,Quantity:quantity,Price:price}}))
-        console.log("CartItems in MenuBox: ",cartitems);
-        console.log("OrderArray: ",orderarr);
-        // removeOrderfromarray(props.name)
+    // dispatch(removeOrder(props.name))
+  }
+  useEffect(() => {
+    setquantity(quantity);
+    // setorder({Name,quantity})
+  }, [quantity, itemName, price]);
+  useEffect(() => {
+    setquantity(0);
+    setselected(false);
+    setadd(false);
+  }, [cart_reset]);
+  //------------------------------------ Handle Add Function ---------------------------------------------------------//
+  function handleadd() {
+    dispatch(increment());
+    setadd(true);
+    // console.log('order: ',order);
+    total = quantity * price;
+    dispatch(reduceamount(prevtotal));
+    dispatch(addamount(total));
+    dispatch(
+      addorders({ ITEM: { Name: itemName, Quantity: quantity, Price: price } })
+    );
+    console.log("CartItems in MenuBox: ", cartitems);
+    console.log("OrderArray: ", orderarr);
+    // removeOrderfromarray(props.name)
 
+    // <Toast/>
+    // console.log("Total: ",total);
+    // ORDER_ARRAY.push(order)
+    // console.log("Order: ",order);
 
-        // <Toast/>
-        // console.log("Total: ",total);
-        // ORDER_ARRAY.push(order)
-        // console.log("Order: ",order);
-        
-        
-        // console.log("Result OrderArray: ",result);
-        // console.log(ORDER_ARRAY);
-        
-    }
-    
+    // console.log("Result OrderArray: ",result);
+    // console.log(ORDER_ARRAY);
+  }
 
-    // const result = useSelector(state=>state.orders.OrderArray)
-    return <>
-    <div className="container-sm ">
+  // const result = useSelector(state=>state.orders.OrderArray)
+  return (
+    <>
+      <div className="container-sm ">
         <div className="row">
-        <div className="container-sm col">
-        <img style={style.image} src={img} alt="image_png"/>
-        <h6 className="w-50 my-2">{props.name}</h6>
-        <span style={{fontSize:'12px',position:'relative',top:'-8px'}}>⭐</span>
-        </div>
-        <div className="container-sm col text-end">
-            <span>₹7.99</span><span style={style.note} className="text-muted"> */piece</span>
+          <div className="container-sm col">
+            <img style={style.image} src={img} alt="image_png" />
+            <h6 className="w-50 my-2">{props.name}</h6>
+            <span
+              style={{ fontSize: "12px", position: "relative", top: "-8px" }}
+            >
+              ⭐
+            </span>
+          </div>
+          <div className="container-sm col text-end">
+            <span>₹7.99</span>
+            <span style={style.note} className="text-muted">
+              {" "}
+              */piece
+            </span>
             {/* <h6 className="text-muted">*min 3pcs required</h6> */}
-            <AddQuantity hide={added} add={handleinc} sub={handledec} remove={handledelete} quantity={quantity}/>
-            {added?<button disabled={(!selected||should_blur)&&true} id="addbtn" className="btn btn-sm my-3 ">✅Added</button>:<button disabled={(!selected||should_blur)&&true} id="addbtn" onClick={handleadd} className="btn btn-sm my-3 ">Add</button>}
+            <AddQuantity
+              hide={added}
+              add={handleinc}
+              sub={handledec}
+              remove={handledelete}
+              quantity={quantity}
+            />
+            {(added && !cart_reset) ? (
+              <button
+                disabled={(!selected || should_blur) && true}
+                id="addbtn"
+                className="btn btn-sm my-3 "
+              >
+                ✅Added
+              </button>
+            ) : (
+              <button
+                disabled={(!selected || should_blur) && true}
+                id="addbtn"
+                onClick={handleadd}
+                className="btn btn-sm my-3 "
+              >
+                Add
+              </button>
+            )}
+          </div>
         </div>
-        </div>
-    </div>
-    
-    <hr/>
-    </>
-}
+      </div>
 
+      <hr />
+    </>
+  );
+};
--- src/cart/cart.jsx
+++ src/cart/cart.jsx
@@ -1,110 +1,106 @@
 import React, { Fragment, useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import {  resetOrderArray } from "../redux/feature/order/Orders";
+import { resetOrderArray } from "../redux/feature/order/Orders";
 import { resetquantity } from "../redux/feature/Quantity/quantitySlice";
 import { informreset } from "../redux/feature/reset/reset";
 import { resettotal } from "../redux/feature/total/totalSlice";
 import { infomcartclick } from "../redux/Global/globalstate";
-import './cart.css'
-export var cartitems = []
+import "./cart.css";
+export var cartitems = [];
 
+export function Cart() {
+  const dispatch = useDispatch();
+  const open = useSelector((state) => state.Gstate.iscartclicked);
+  const [shouldopen, setopen] = useState(false);
+  cartitems = useSelector((state) => state.orders.OrderArray);
+  // console.log("Index of Name: ",);
+  console.log("CartItems: ", cartitems);
+  // const dispatch = useDispatch()
+  useEffect(() => {
+    setopen(open);
+  }, [open]);
+  function resetcart() {
+    dispatch(resettotal());
+    dispatch(resetOrderArray());
+    dispatch(resetquantity());
+    dispatch(informreset());
+    setopen(false);
+    dispatch(infomcartclick());
+  }
+  // for (let index = 0; index < cartitems.length; index++) {
+  //     // console.log("Cartitems individual: ",cartitems[index]);
+  //     console.log(cartitems[index][0].Name)
 
-export function Cart(){
-    const dispatch = useDispatch()
-    const open = useSelector(state=>state.Gstate.iscartclicked)
-    const[shouldopen,setopen] = useState(false)
-    cartitems = useSelector(state=>state.orders.OrderArray)
-    // console.log("Index of Name: ",);
-    console.log("CartItems: ",cartitems);
-    // const dispatch = useDispatch()
-    useEffect(()=>{
-        setopen(open)
-    },[open])
-    function resetcart(){
-        dispatch(resettotal())
-        dispatch(resetOrderArray())
-        dispatch(resetquantity())
-        dispatch(informreset())
-        setopen(false)
-        dispatch(infomcartclick())
-    }
-    // for (let index = 0; index < cartitems.length; index++) {
-    //     // console.log("Cartitems individual: ",cartitems[index]);
-    //     console.log(cartitems[index][0].Name)
-        
-    // }
-   
-//     const removeOrderfromarray = (event,itemname)=>{
-//     const result = []
-//     for(let i=0;i<cartitems.length;i++){
-//         console.log("Cartitems in loop: ",cartitems[i].ITEM.Name);
-//         if(cartitems[i].ITEM.Name!==itemname){
-//             result.push(cartitems[i]);
-//             break;
-            
-//         }
-//         console.log("Result array:  ",result);
-//     }
-//     dispatch(removeOrder(result))
-//     console.log("Item has been removed!!");
-//     // dispatch(removeOrder(result))
-    
-// }
-const style={
-    btn:{
-        margin:0,
-        border:'none',
-        borderRadius:'10px',
-        backgroundColor:"grey",
-        width:'50px',
-        background:'transparent'
-        
-    
+  // }
+
+  //     const removeOrderfromarray = (event,itemname)=>{
+  //     const result = []
+  //     for(let i=0;i<cartitems.length;i++){
+  //         console.log("Cartitems in loop: ",cartitems[i].ITEM.Name);
+  //         if(cartitems[i].ITEM.Name!==itemname){
+  //             result.push(cartitems[i]);
+  //             break;
+
+  //         }
+  //         console.log("Result array:  ",result);
+  //     }
+  //     dispatch(removeOrder(result))
+  //     console.log("Item has been removed!!");
+  //     // dispatch(removeOrder(result))
+
+  // }
+  const style = {
+    btn: {
+      margin: 0,
+      border: "none",
+      borderRadius: "10px",
+      backgroundColor: "grey",
+      width: "50px",
+      background: "transparent",
     },
-    restbtn:{
-        border:'none',
-        borderRadius:'20px',
-        height:'20px',
-        fontSize:'13px',
-        fontWeight:'bold',
-        marginTop:'0.3rem',
-        backgroundColor:'lightblue',
-    }
-}
+    restbtn: {
+      border: "none",
+      borderRadius: "20px",
+      height: "20px",
+      fontSize: "13px",
+      fontWeight: "bold",
+      marginTop: "0.3rem",
+      backgroundColor: "lightblue",
+    },
+  };
 
-    return <>
-    { shouldopen?<div id="cart" className="container">
-        <div className="container d-flex justify-content-between">
-        <h1>Cart</h1>
-        <button style={style.restbtn} onClick={resetcart}>Reset Cart</button>
-        </div>
-        <table className="">
+  return (
+    <>
+      {shouldopen ? (
+        <div id="cart" className="container">
+          <div className="container d-flex justify-content-between">
+            <h1>Cart</h1>
+            <button style={style.restbtn} onClick={resetcart}>
+              Reset Cart
+            </button>
+          </div>
+          <table className="">
             <thead>
-                <tr>
-                    <th scope="col">Item</th>
-                    <th scope="col">Quantity</th>
-                </tr>
+              <tr>
+                <th scope="col">Item</th>
+                <th scope="col">Quantity</th>
+              </tr>
             </thead>
             <tbody>
-                {cartitems.map((i)=>{
-                    return (
-                        <Fragment>
-                            <tr key={i.id}>
-                                <td>{i.ITEM.Name}</td>
-                                <td>{i.ITEM.Quantity}</td>
-    
-                            </tr>
-                        </Fragment>
-                    )
-                })}
+              {cartitems.map((i) => {
+                return (
+                  <Fragment>
+                    <tr key={i.id}>
+                      <td>{i.ITEM.Name}</td>
+                      <td>{i.ITEM.Quantity}</td>
+                    </tr>
+                  </Fragment>
+                );
+              })}
             </tbody>
-        </table>
-
-    </div>:null
-
-    }
-    
+          </table>
+        </div>
+      ) : null}
     </>
+  );
 }
-
-
