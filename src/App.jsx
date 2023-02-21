9298b4f955079f0c4cb20550c0135c28ce5ef33d
Gokul001 <gokuljune2003@gmail.com>
2023-02-21 20:47:03 +0530

    removed privateroute for testing in live without backend

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,14 @@
-import React, {  useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
-import {Navigate }from 'react-router-dom';
+import { Navigate } from "react-router-dom";
 import PrivateRoute from "./Auth";
-import { BrowserRouter, Routes, Route, useNavigate,Redirect} from "react-router-dom";
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  useNavigate,
+  Redirect,
+} from "react-router-dom";
 // import OrderType from "./OrderType";
 import Homeorder from "./Homeorder";
 import Loginpage from "./Loginorder/login";
@@ -10,22 +16,24 @@ import Checkout from "./checkout/checkout";
 function App() {
   return (
     <>
-    <BrowserRouter>
-    <Routes>
-      <Route path="/" element={
-      
-        <PrivateRoute>
-           <Homeorder/>
-         </PrivateRoute>
-     }/>
-      <Route path="login" element={<Loginpage/>}/>
-      <Route path="order">
-        <Route path=":orderid" element={<Checkout/>} />
-      </Route>
-    </Routes>
-    </BrowserRouter>
+      <BrowserRouter>
+        <Routes>
+          <Route
+            path="/"
+            element={
+              // <PrivateRoute>
+              <Homeorder />
+              //  </PrivateRoute>
+            }
+          />
+          <Route path="login" element={<Loginpage />} />
+          <Route path="order">
+            <Route path=":orderid" element={<Checkout />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </>
-  )};
-
+  );
+}
 
 export default App;
